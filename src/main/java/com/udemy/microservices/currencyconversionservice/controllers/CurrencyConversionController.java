a4c481e9af90da72cfeb0116c61bd158cb36d7ce
SergeA <ronnieo@gmail.com>
2019-06-02 12:08:42 -0400

    Implemented integration with currency-exchange-service

--- src/main/java/com/udemy/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/com/udemy/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,11 +1,15 @@
 package com.udemy.microservices.currencyconversionservice.controllers;
 
 import com.udemy.microservices.currencyconversionservice.domain.CurrencyConversionBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,11 +18,27 @@ public class CurrencyConversionController {
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
                                                   @PathVariable String to,
                                                   @PathVariable BigDecimal quantity) {
+
+        Map<String, String> uriParams = new HashMap<>();
+        uriParams.put("from", from);
+        uriParams.put("to", to);
+        ResponseEntity<CurrencyConversionBean> response = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class,
+                uriParams
+        );
+
+        CurrencyConversionBean result = response.getBody();
+
+        if(result == null) {
+            return CurrencyConversionBean.builder().build();
+        }
+
         return CurrencyConversionBean.builder()
                 .id(1L).from(from).to(to)
                 .quantity(quantity)
-                .conversionMultiple(BigDecimal.ONE)
-                .totalCalculatedAmount(quantity)
+                .conversionMultiple(result.getConversionMultiple())
+                .totalCalculatedAmount(quantity.multiply(result.getConversionMultiple()))
                 .build();
     }
 }
