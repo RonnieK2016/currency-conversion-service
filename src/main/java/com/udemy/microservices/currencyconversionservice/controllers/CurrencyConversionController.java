fe848bcfee8c2520278145fe03aa1ddad5064367
SergeA <ronnieo@gmail.com>
2019-06-02 12:37:48 -0400

    Implemented Feign Support for currency-exchange-service

--- src/main/java/com/udemy/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/com/udemy/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.udemy.microservices.currencyconversionservice.controllers;
 
 import com.udemy.microservices.currencyconversionservice.domain.CurrencyConversionBean;
+import com.udemy.microservices.currencyconversionservice.proxies.CurrencyExchangeServiceProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,12 @@ import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    private CurrencyExchangeServiceProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy proxy) {
+        this.proxy = proxy;
+    }
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
@@ -29,6 +36,25 @@ public class CurrencyConversionController {
         );
 
         CurrencyConversionBean result = response.getBody();
+
+        if(result == null) {
+            return CurrencyConversionBean.builder().build();
+        }
+
+        return CurrencyConversionBean.builder()
+                .id(1L).from(from).to(to)
+                .quantity(quantity)
+                .conversionMultiple(result.getConversionMultiple())
+                .totalCalculatedAmount(quantity.multiply(result.getConversionMultiple()))
+                .build();
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+                                                  @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean result = proxy.retrieveExchangeValue(from, to);
 
         if(result == null) {
             return CurrencyConversionBean.builder().build();
